b051616097dbd52734376d30ca01fcdb97b6eb3e
Tom Davy <tomd200@mac.com>
2017-11-21 16:52:47 +0000

    end of day

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,12 +22,15 @@ from django.views import static
 from accounts.views import get_index
 from accounts import urls as accounts_urls
 from products import urls as products_urls
+from cart import urls as cart_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r"^$", get_index, name="get_index"),
+    url(r"^$", get_index, name="index"),
     url(r"^accounts/", include(accounts_urls)),
     url(r"^products/", include(products_urls)),
+    url(r'^cart/', include(cart_urls)),
+    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
 
 
