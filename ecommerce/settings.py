c33a6f69c82dd385ca114806409794dd7a26fb22
Tom Davy <tomd200@mac.com>
2017-11-28 16:25:49 +0000

    deploy

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY=os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.eviron.get("DEBUG", True)
+DEBUG = os.environ.get("DEBUG", True)
 
 # ALLOWED_HOSTS = [
 # "stream3-ecommerce-tomd100.c9users.io",
